150ae28cff4f9c9dab6c82256fb38c81843ce81b
Mezozzoi <64903418+Mezozzoi@users.noreply.github.com>
2024-03-02 21:11:33 +0200

    Add project files.

---
+++ Lab1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DPGI
+{
+    class Program
+    {
+        /// <summary>
+        /// Main funtion.
+        /// </summary>
+        /// <param name="args">App run arguments.</param>
+        static void Main(string[] args)
+        {
+            Hello.Print();
+        }
+    }
+
+    class Hello
+    {
+        public static void Print()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Lab1/readme.txt
@@ -0,0 +1,4 @@
+Repository for labs
+
+Versions:
+v1 - 15.02.2024
